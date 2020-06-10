0a2d4b0371a26e8a8d13e1d5fcc240b6754d2e0c
vladkovalevich <64867756+vladkovalevich@users.noreply.github.com>
2020-06-10 10:24:52 +0300

    addNew

---
+++ 15/15/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15
+{
+    class Class1
+    {
+        int var1 = 0;
+
+        void func1()
+        {
+            var1 = 10;
+        }
+
+        void func2()
+        {
+            System.Diagnostics.Debug.WriteLine(var1);
+        }
+    }
+}
