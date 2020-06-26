a8c489bd9d16203e2b777f6fb0e7a5313b6fc570
vladkovalevich <64867756+vladkovalevich@users.noreply.github.com>
2020-06-26 15:00:03 +0300

    .

--- Tasks/Tasks/MainWindow.xaml.cs
+++ Tasks/Tasks/MainWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace Tasks
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            // 6 8
            //https://acmp.ru/index.asp?main=task&id_task=33
             string[] A = Input.Text.Split(' ');
             int Larry = 10;
@@ -36,6 +37,7 @@ namespace Tasks
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
+            //Привет
             if(Convert.ToInt32(Input.Text) >100)
                Output.Text = "Error";
             else
@@ -44,6 +46,7 @@ namespace Tasks
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
+            //5 1 0 1 1 0
             //https://acmp.ru/index.asp?main=task&id_task=106
             string[] A = Input.Text.Split(' ');
             int n = 0;
