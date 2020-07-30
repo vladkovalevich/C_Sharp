8e3396ca06f1e4485058e3eed09cfe89013a63e2
vladkovalevich <64867756+vladkovalevich@users.noreply.github.com>
2020-07-30 10:19:21 +0300

    ...

---
+++ CosoleTask/CosoleTask/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace CosoleTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Следующее
+            /*int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Convert.ToString(n + 1));*/
+
+            //Предыдущее
+            /*int n = Convert.ToInt32(Console.ReadLine())
+             * Console.WriteLine(Convert.ToString(n - 1));*/
+
+            //Разница чисел 
+            /*string s = Console.ReadLine();
+            string[] n = s.Split("");
+            Console.WriteLine(Convert.ToString(Convert.ToInt32(n[0]) - Convert.ToInt32(n[1])));*/
+
+            //P и S
+            /*double n = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Convert.ToString(n * 4 + " " + n * n));
+
+            Console.ReadLine();*/
+
+            //19 todo 30%
+            string a = Console.ReadLine();
+            int result = 0;
+            for (int i = 0; a.Length / 2 < 0; i++)
+            {
+                if (a[i] == a[-i - 1]) 
+                {
+                    result += 1;
+                }
+            }
+            if (a.Length % 2 != 0) {
+                result += 1;
+            }
+            Console.WriteLine(result);
+            //8803 100%
+            string a1 = Console.ReadLine();
+            Console.WriteLine(Convert.ToInt32(a1) + 1);
+            //8809 100%
+            string a2 = Console.ReadLine();
+            string[] d = a2.Split(" ");
+            int resultd = Convert.ToInt32(d[0]) - Convert.ToInt32(d[1]);
+            Console.WriteLine(resultd);
+            //8806
+            string a3 = Console.ReadLine();
+            string[] d1 = a3.Split(" ");
+            int resultd1 = Convert.ToInt32(d1[0]) + Convert.ToInt32(d1[1]);
+            Console.WriteLine(resultd1);
+        }
+    }
+}
