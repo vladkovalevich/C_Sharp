f56ff6f22f321c81f6aa97faa67905eb8bec4a2c
vladkovalevich <64867756+vladkovalevich@users.noreply.github.com>
2020-08-12 15:09:25 +0300

    ....

--- CosoleTask/CosoleTask/Program.cs
+++ CosoleTask/CosoleTask/Program.cs
@@ -2,93 +2,146 @@
 
 namespace CosoleTask
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //8804 70 https://www.e-olymp.com/ru/problems/8804
-            string a = Console.ReadLine();
-            string b = Console.ReadLine();
-            Console.WriteLine(Convert.ToInt32(a)+Convert.ToInt32(b));
-    
-            //500 https://www.e-olymp.com/ru/problems/500
-            string a1 = Console.ReadLine();
-            string v = Console.ReadLine();
-            string[] b1 = v.Split("");
-            if (Convert.ToInt32(b[0]+b[1]+b[2]) == 3000)
-            {
-                 Console.WriteLine("Error");
-            }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//1 new
+			/*
+			string a = Console.ReadLine();
+			string[] q = a.Split(" ");
+			Console.WriteLine(q);
+			*/
 
-            //Следующее
-            /*int n = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(Convert.ToString(n + 1));*/
+			//19 
+			/*
+			string a = Console.ReadLine();
+			int result = 0;
+			for (int i = 0; a.Length / 2 > i; i++)
+			{
+			    if (a[i] == a[a.Length - i - 1])
+				{
+					result += 1;
+				}
+			}
+			if (a.Length % 2 != 0)
+			{
+				result += 1;
+			}
+			Console.WriteLine(result);
+			*/
 
-            //Предыдущее
-            /*int n = Convert.ToInt32(Console.ReadLine())
-             * Console.WriteLine(Convert.ToString(n - 1));*/
+			//500 new
+			/*int hz = Convert.ToInt32(Console.ReadLine());
+			string[] z1 = Console.ReadLine().Split(" ");
+			string[] z2 = Console.ReadLine().Split(" ");
 
-            //Разница чисел 
-            /*string s = Console.ReadLine();
-            string[] n = s.Split("");
-            Console.WriteLine(Convert.ToString(Convert.ToInt32(n[0]) - Convert.ToInt32(n[1])));*/
+			int L = Convert.ToInt32(z1[0]);
+			int W = Convert.ToInt32(z1[0]);
+			int H = Convert.ToInt32(z1[0]);
 
-            //P и S
-            /*double n = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine(Convert.ToString(n * 4 + " " + n * n));
+			int L1 = Convert.ToInt32(z2[0]);
+			int W1 = Convert.ToInt32(z2[0]);
+			int H1 = Convert.ToInt32(z2[0]);
 
-            Console.ReadLine();
+			if (L >= 1000 || W >= 1000 || H >= 1000) 
+			{
+				Console.WriteLine("error");
+				return;
+			}
+			if (L1 >= 1000 || W1 >= 1000 || H1 >= 1000)
+			{
+				Console.WriteLine("error");
+				return;
+			}
+			double s = 2 * (W * L) * H;
+			s = s / 16;
+			double result = Math.Round(s);
+			*/
 
-            //19 todo51
+			//8801
+			/*
+			int n = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine(Convert.ToString(n + 1));
+			*/
 
-            string a = Console.ReadLine();
-            int result = 0;
-            for (int i = 0; a.Length / 2 > i; i++)
-            {
-               if (a[i] == a[a.Length - i - 1])
-                    {
-                        result += 1;
-                    }
-                }
-                if (a.Length % 2 != 0)
-                {
-                    result += 1;
-                }
-                Console.WriteLine(result);
-            
+			//8802
+			/*
+			int n = Convert.ToInt32(Console.ReadLine())
+			Console.WriteLine(Convert.ToString(n - 1));
+			*/
 
-    //8803 100%
-    //string a1 = Console.ReadLine();
-    //Console.WriteLine(Convert.ToInt32(a1) + 1);
-    //8809 100%
-    //string a2 = Console.ReadLine();
-    //string[] d = a2.Split(" ");
-    //int resultd = Convert.ToInt32(d[0]) - Convert.ToInt32(d[1]);
-    //Console.WriteLine(resultd);
-    //8806 100%
-    //string a3 = Console.ReadLine();
-    //string[] d1 = a3.Split(" ");
-    //int resultd1 = Convert.ToInt32(d1[0]) + Convert.ToInt32(d1[1]);
-    //Console.WriteLine(resultd1);
-    //8811 100%
-    //string a4 = Console.ReadLine();
-    //string[] d2 = a4.Split(" ");
-    //int resultd2 = Convert.ToInt32(d2[0]) * Convert.ToInt32(d2[1]);
-    //Console.WriteLine(resultd2);
-    //8806 100%
-    //string a4 = Console.ReadLine();
-    //string[] d2 = a4.Split(" ");
-    //int resultd2 = Convert.ToInt32(d2[0]) - Convert.ToInt32(d2[1]);
-    //Console.WriteLine(resultd2);
-    //
-           //string a = Console.ReadLine();
-            //string v = Console.ReadLine();
-            //string[] b = v.Split("");
-           // if (Convert.ToInt32(b[0]+b[1]+b[2]) == 3000) {
-          //      Console.WriteLine("Error");
-         //   }
-         */
+			//8803
+			/*
+			string a1 = Console.ReadLine();
+			Console.WriteLine(Convert.ToInt32(a1) + 1);
+			*/
 
-        }
-    }
+			//8804
+			/*
+			string a = Console.ReadLine();
+			string b = Console.ReadLine();
+			Console.WriteLine(Convert.ToInt32(a)+Convert.ToInt32(b));
+			*/
+
+			//8805 
+			/*
+			string a4 = Console.ReadLine();
+			string[] d2 = a4.Split(" ");
+			int resultd2 = Convert.ToInt32(d2[0]) + Convert.ToInt32(d2[1]);
+			Console.WriteLine(resultd2);
+			*/
+
+			//8806
+			/*
+			string a3 = Console.ReadLine();
+			string[] d1 = a3.Split(" ");
+			int resultd1 = Convert.ToInt32(d1[0]) + Convert.ToInt32(d1[1]);
+			Console.WriteLine(resultd1);
+			*/
+
+			//8807
+			/*
+			string a2 = Console.ReadLine();
+			Console.WriteLine(Convert.ToInt32(a2) - (Convert.ToInt32(a2) * 2));
+			*/
+
+			//8808 new
+			/*
+			string a = Console.ReadLine();
+			string b = Console.ReadLine();
+			Console.WriteLine(Convert.ToInt32(a) - Convert.ToInt32(b));
+			*/
+
+			//8809 
+			/*
+			string s = Console.ReadLine();
+			string[] n = s.Split(" ");
+			Console.WriteLine(Convert.ToString(Convert.ToInt32(n[0]) - Convert.ToInt32(n[1])));
+			*/
+
+			//8811
+			/*
+			string a4 = Console.ReadLine();
+			string[] d2 = a4.Split(" ");
+			int resultd2 = Convert.ToInt32(d2[0]) * Convert.ToInt32(d2[1]);
+			Console.WriteLine(resultd2);
+			*/
+
+			//8814
+			/*
+			double n = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine(Convert.ToString(n * 4 + " " + n * n));
+			Console.ReadLine();
+			*/
+
+			//8816
+			/*
+			string a = Console.ReadLine();
+			string b = Console.ReadLine();
+			Console.WriteLine(Convert.ToInt32(a) ^ Convert.ToInt32(b));
+			*/
+
+		}
+	}
 }
